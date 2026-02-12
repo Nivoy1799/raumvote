f74e045ad076519a56e319f055f78c9deb754c7e
Jovin Risch <jovin.risch@ost.ch>
2026-02-12 16:39:46 +0100

    new tree + iphone impr 33

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <meta name="apple-mobile-web-app-title" content="RaumVote" />
+      <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+    </>
+  );
+}
