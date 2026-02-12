8689c90e450786e841668b117ba42c5b6ec7e36b
Jovin Risch <jovin.risch@ost.ch>
2026-02-12 14:46:24 +0100

    iphone support

---
+++ app/meta.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <meta name="theme-color" content="#000000" />
+    </>
+  );
+}
